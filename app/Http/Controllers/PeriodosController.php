4a1eaf937111cf9cef27b0c1912c5eccd17d17e9
Joseph Silva <joseph.300604@bento.local>
2022-08-31 08:07:39 -0300

    criada tabela periodos

---
+++ app/Http/Controllers/PeriodosController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PeriodosController extends Controller
+{
+    //
+    function index(){
+
+    }
+
+    function create(){
+
+    }
+    function store(){
+
+    }
+    function edit(){
+
+    }
+    function update(){
+
+    }
+    function delete(){
+
+    }
+}
