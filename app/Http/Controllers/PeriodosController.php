c69626f81afb4f835c6bb208c745f7884747a798
Victor Mazutti <victormazutti05@gmail.com>
2022-09-11 21:26:04 -0300

    Arrumando navbar errada e erro em período

--- app/Http/Controllers/PeriodosController.php
+++ app/Http/Controllers/PeriodosController.php
@@ -17,15 +17,13 @@ class PeriodosController extends Controller
             ")
             ->get();
 
-    function create(){
-
-
         return view('periodos.index', ['periodos' => $periodos]);
     }
+    
     public function create()
     {
         return view('periodos.create');
-        
+
     }
     public function show($id)
     {
